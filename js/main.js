f633fa48cb8032fc16b934c731d2c4a32da0d463
Alexander Van Maele <alexander@atlesque.com>
2019-01-07 10:46:19 +0100

    Tie JS files into modules

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
+import settings from './settings'
+
 let peerConnection
+let socket
 
 function connect () {
   document.getElementById('join-chat').disabled = true
   log('Connecting to chat server...')
-
+  socket = io(settings.chatServerURL)
 }
 
 function initVideoConnection () {
-  peerConnection = new Peer(ICEConfig)
+  peerConnection = new Peer(settings.ICEConfig)
 }
 
 function log(text) {
   document.getElementById('chat').insertAdjacentHTML('beforeend', `<li class='chat__text--log'>${text}</li>`)
 }
+
+export { connect, initVideoConnection }
