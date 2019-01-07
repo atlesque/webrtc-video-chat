454a00ba25b919f864f79a142293d7f46cc6d616
Alexander Van Maele <alexander@atlesque.com>
2019-01-07 10:29:40 +0100

    Add logging function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let peerConnection
 
+function connect () {
+  document.getElementById('join-chat').disabled = true
+  log('Connecting to chat server...')
+
+}
+
 function initVideoConnection () {
   peerConnection = new Peer(ICEConfig)
+}
+
+function log(text) {
+  document.getElementById('chat').insertAdjacentHTML('beforeend', `<li class='chat__text--log'>${text}</li>`)
 }
